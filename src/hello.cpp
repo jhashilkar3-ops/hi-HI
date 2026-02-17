156e3edfbeb0c4600dfeb9536ededb844511d133
Jiya Hashilkar <jhashilkar3@gmail.com>
2026-02-16 18:42:16 -0600

    Update hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,27 @@
-#include <string>
+#include "hello.hpp"
+#include <cctype>
+using namespace std;
+int strcmp_case_insensitive(const string& s1, const string& s2)
+{
+    size_t i = 0;
 
-std::string hello() {
-   return "Hello World!";
+    while (i < s1.length() && i < s2.length())
+    {
+        char c1 = tolower(s1[i]);
+        char c2 = tolower(s2[i]);
+
+        if (c1 != c2)
+        {
+            return c1 - c2;
+        }
+
+        i++;
+    }
+
+    if (s1.length() == s2.length())
+        return 0;
+    else if (s1.length() < s2.length())
+        return -1;
+    else
+        return 1;
 }

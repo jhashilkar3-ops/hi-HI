863b4c56ec2d33517fbc25b2fe7004364c5a9ff7
Jiya Hashilkar <jhashilkar3@gmail.com>
2026-02-16 18:45:31 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
 #include "src/hello.hpp"
 
-int main() {
-  // Your driver program goes here
+int main()
+{
+    std::cout << "Comparing \"String one\" and \"string two\": "
+              << strcmp_case_insensitive("banana", "APPLE") << std::endl;
+
+    std::cout << "Comparing \"String one\" and \"string one\": "
+              << strcmp_case_insensitive("HI", "hi") << std::endl;
+
+    std::cout << "Comparing \"String two\" and \"string one\": "
+              << strcmp_case_insensitive("Cat", "dog ") << std::endl;
+
+
+    return 0;
 }
